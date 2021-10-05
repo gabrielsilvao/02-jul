656c1aa30b1c0e215207a1d0bad932ef02cd98dd
loktwo <loktwo@outlook.com>
2021-10-05 16:35:33 -0300

    october simulated

--- main.tf
+++ main.tf
@@ -4,11 +4,10 @@ provider "azurerm" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "TFNet" {
-    name                = "terraform_vnet"
-    address_space       = ["10.3.0.0/16"]
+    name                = "<UNIQUE_STORAGE_ACCOUNT_NAME>"
+    address_space       = ["10.0.0.0/16"]
     location            = "East US"
-    resource_group_name = "1-1c02bce7-playground-sandbox"
-
+    resource_group_name = "<RESOURCE_GROUP_NAME>
     tags = {
         environment = "Terraform VNET"
     }
@@ -16,26 +15,23 @@ resource "azurerm_virtual_network" "TFNet" {
 # Create subnet
 resource "azurerm_subnet" "tfsubnet" {
     name                 = "default"
-    resource_group_name = "1-1c02bce7-playground-sandbox"
+    resource_group_name = "<RESOURCE_GROUP_NAME>"
     virtual_network_name = azurerm_virtual_network.TFNet.name
-    address_prefix       = "10.3.0.0/24"
+    address_prefix       = "10.0.1.0/24"
 }
-
 #Deploy Public IP
 resource "azurerm_public_ip" "example" {
   name                = "pubip1"
   location            = "East US"
-  resource_group_name = "1-1c02bce7-playground-sandbox"
+  resource_group_name = "<RESOURCE_GROUP_NAME>"
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
-
 #Create NIC
 resource "azurerm_network_interface" "example" {
-  name                = "nameforinterface"  
+  name                = "Enter name for this NIC"  
   location            = "East US"
-  resource_group_name = "1-1c02bce7-playground-sandbox"
-
+  resource_group_name = "<RESOURCE_GROUP_NAME>"
     ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.tfsubnet.id 
@@ -43,38 +39,33 @@ resource "azurerm_network_interface" "example" {
     public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
-
 #Create Boot Diagnostic Account
 resource "azurerm_storage_account" "sa" {
-  name                     = "diagnostaccount" 
-  resource_group_name      = "1-1c02bce7-playground-sandbox"
+  name                     = "Enter Name for Diagnostic Account" 
+  resource_group_name      = "<RESOURCE_GROUP_NAME>"
   location                 = "East US"
    account_tier            = "Standard"
    account_replication_type = "LRS"
-
    tags = {
     environment = "Boot Diagnostic Storage"
     CreatedBy = "Admin"
    }
   }
-
 #Create Virtual Machine
 resource "azurerm_virtual_machine" "example" {
-  name                  = "terraform-vm"  
+  name                  = "Enter AzureVM Name"  
   location              = "East US"
-  resource_group_name   = "1-1c02bce7-playground-sandbox"
+  resource_group_name   = "<RESOURCE_GROUP_NAME>"
   network_interface_ids = [azurerm_network_interface.example.id]
   vm_size               = "Standard_B1s"
   delete_os_disk_on_termination = true
   delete_data_disks_on_termination = true
-
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
-
   storage_os_disk {
     name              = "osdisk1"
     disk_size_gb      = "128"
@@ -82,19 +73,16 @@ resource "azurerm_virtual_machine" "example" {
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
     computer_name  = "Enter Server Name"
-    admin_username = "azureuser"
-    admin_password = "Seletiva#39"
+    admin_username = "vmadmin"
+    admin_password = "Password12345!"
   }
-
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
 boot_diagnostics {
         enabled     = "true"
         storage_uri = azurerm_storage_account.sa.primary_blob_endpoint
     }
-}
+} 
